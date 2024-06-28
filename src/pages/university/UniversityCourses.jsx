35e28910715e05298db3fa4b77fba138d2032967
Dev Kapadia <devkapadia123@gmail.com>
2024-06-28 18:11:04 +0530

    courses page initialization

---
+++ src/pages/university/UniversityCourses.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UniversityLayout from "../../layout/UniversityLayout";
+
+const UniversityCourses = () => {
+  return <UniversityLayout>CourseDetails</UniversityLayout>;
+};
+
+export default UniversityCourses;
