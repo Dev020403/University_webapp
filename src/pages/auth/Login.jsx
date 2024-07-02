7e432c04de79a70bf91863850f5b64967cfa11b5
Dev Kapadia <devkapadia123@gmail.com>
2024-07-02 10:10:23 +0530

    eye added in login

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,15 +1,22 @@
-import React from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import React, { useState } from "react";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../redux/authSlice";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
+import TextInputField from "../../components/auth/TextInputField"; // Adjust the import path as needed
 
 const Login = () => {
   const dispatch = useDispatch();
   const { loading, error } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   const validationSchema = Yup.object().shape({
     username: Yup.string().required("Username is required"),
@@ -20,7 +27,7 @@ const Login = () => {
     username: "",
     password: "",
   };
-  const navigate = useNavigate();
+
   const handleSubmit = (values, { setSubmitting }) => {
     dispatch(loginUser(values))
       .unwrap()
@@ -51,38 +58,20 @@ const Login = () => {
         >
           {({ isSubmitting }) => (
             <Form className="space-y-6">
-              <div className="flex flex-col gap-2">
-                <label htmlFor="username" className="font-semibold">
-                  Username
-                </label>
-                <Field
-                  type="text"
-                  name="username"
-                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
-                  placeholder="Username"
-                />
-                <ErrorMessage
-                  name="username"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
-              </div>
-              <div className="flex flex-col gap-2">
-                <label htmlFor="password" className="font-semibold">
-                  Password
-                </label>
-                <Field
-                  type="password"
-                  name="password"
-                  className="border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
-                  placeholder="********"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-red-500 text-sm"
-                />
-              </div>
+              <TextInputField
+                label="Username"
+                name="username"
+                placeholder="Username"
+              />
+              <TextInputField
+                label="Password"
+                name="password"
+                type="password"
+                placeholder="********"
+                isPassword
+                showPassword={showPassword}
+                toggleShowPassword={toggleShowPassword}
+              />
               <button
                 className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition-colors w-full"
                 type="submit"
